ab8446bf33771ea5d36154a447acada3622cad0d
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-07-23 15:03:39 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -57,5 +57,5 @@ const dropdown = document.getElementById("breed-dropdown");
 dropdown.addEventListener('change', function(e){
   const letter = e.target.value;
   const filteredBreeds = allBreeds.filter((breed => breed.startsWith(letter)));
-  container.innerHTML = createBreedsFromArray(filteredBreeds);
+  createBreedsFromArray(filteredBreeds);
 });
