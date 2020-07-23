420bc466b406845faa53ee937fe7623cf660de31
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-07-23 15:10:08 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -61,5 +61,5 @@ dropdown.addEventListener('change', function(e){
   for(let i = 0; i < allLi.length; i++){
     allLi[i].remove();
   }
-  createBreedsFromArray(filteredBreeds);
+  console.log(createBreedsFromArray(filteredBreeds));
 });
